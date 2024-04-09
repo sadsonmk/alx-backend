752184bcc9e88dcedf5c5aec4fb1ddccb8830e4c
Sadsonmk <sadsonmkumira@yahoo.com>
2024-04-09 10:40:59 -0700

    Adding file

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -4,13 +4,6 @@
 from flask import Flask, render_template, request, g
 from flask_babel import Babel
 
-users = {
-    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
-    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
-    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
-    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
-}
-
 
 class Config:
     """configures the language and locale for the app"""
@@ -22,6 +15,23 @@ class Config:
 app = Flask(__name__)
 app.config.from_object(Config)
 babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """sets the locale for the app"""
+    locale = request.args.get('locale')
+    if locale and locale in app.config['LANGUAGES']:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
 
 
 def get_user(user_id):
@@ -37,15 +47,6 @@ def before_request():
         set it as a global on flask.g.user
     """
     setattr(g, 'user', get_user(request.args.get('login_as', 0)))
-
-
-@babel.localeselector
-def get_locale() -> str:
-    """sets the locale for the app"""
-    locale = request.args.get('locale')
-    if locale and locale in app.config['LANGUAGES']:
-        return locale
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/', strict_slashes=False)
