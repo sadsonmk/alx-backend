4620c7d4b7daa6c5c86fb60f06520b8a73e5f57b
Sadsonmk <sadsonmkumira@yahoo.com>
2024-04-09 04:44:04 -0700

    creating the basic flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -5,10 +5,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
     """view for the home page"""
     return render_template("0-index.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
