6f29dbf7f4700a58f4f5b7f0e46e73c5df4ffd1c
Sadsonmk <sadsonmkumira@yahoo.com>
2024-04-09 06:58:04 -0700

    Adding files 2-app.py

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -13,7 +13,7 @@ class Config:
 
 
 @babel.localeselector
-def get_locale():
+def get_locale() -> str:
     """sets the locale for the app"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
