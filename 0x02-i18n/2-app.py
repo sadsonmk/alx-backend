a71f1dc3025be1776b1966b67cde1144534b113b
Sadsonmk <sadsonmkumira@yahoo.com>
2024-04-09 06:37:35 -0700

    Get locale from request

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """This module creates a flask app"""
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_babel import Babel, localeselector
 
 
